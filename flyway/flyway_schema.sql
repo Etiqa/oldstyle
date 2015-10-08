28aab1baca7355eb3acd89069bdd349552cf723a
Fabio Pulvirenti <fabio@hoverstate.com>
2015-10-08 15:49:53 +0200

    added file schema

---
+++ flyway/flyway_schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS flyway_schema (
+  `version_rank` int(11) NOT NULL,
+  `installed_rank` int(11) NOT NULL,
+  `version` varchar(50) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `script` varchar(1000) NOT NULL,
+  `info` varchar(1000) NOT NULL,
+  `success` tinyint(1) NOT NULL,
+  PRIMARY KEY (`version`),
+  KEY `schema_version_vr_idx` (`version_rank`),
+  KEY `schema_version_ir_idx` (`installed_rank`),
+  KEY `schema_version_s_idx` (`success`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
